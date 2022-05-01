0805f9f9ea97e60e376b65ef4292fa57c8841681
varitant <varitanta@gmail.com>
2022-05-01 17:40:05 +0530

    varitant

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 function App() {
   return <div className="App">
     <div className="bg-red-300">Freshers 2022 Arun Naman</div>
+    <div className="bg-red-300">Freshers 2022 Varitant</div>
   </div>;
 }
 
