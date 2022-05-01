313d39e6a20a1fee6de1f2327ac5375feda9d2f6
Arun Shekhar <arunshekhar001@gmail.com>
2022-05-01 22:38:48 +0530

    init 2

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 
 function App() {
   return <div className="App">
-    <div className="bg-red-300">Freshers</div>
+    <div className="bg-red-300">Freshers 2022</div>
   </div>;
 }
 
