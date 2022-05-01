ef2c5d6afde2327b4a4b00d77d90612243f86ea5
Nikhil Tiwari <nikhiltiwari1903@gmail.com>
2022-05-01 23:15:30 +0530

    nikhil

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 function App() {
   return <div className="App">
     <div className="bg-red-300">Freshers</div>
+    <div className="bg-red-300">Freshers check temp nikhil</div>
   </div>;
 }
 
